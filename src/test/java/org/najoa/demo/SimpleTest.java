3c78ecc61a8e6d97652cf916b33444e78e5c9e3d
Najoa Asreen <najoa06@gmail.com>
2025-01-20 09:54:27 -0500

    Add logging support using lof4j2

--- src/test/java/org/najoa/demo/SimpleTest.java
+++ src/test/java/org/najoa/demo/SimpleTest.java
@@ -1,5 +1,8 @@
-package org.najoa;
+package org.najoa.demo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.najoa.WebDriverSetup;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -7,22 +10,26 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SimpleTest {
-
+    private static final Logger logger = LogManager.getLogger(SimpleTest.class);
     private WebDriver driver;
 
     @BeforeTest
     public void setUp() {
+        this.logTest();
+        logger.info("Initialize WebDriver");
         // Initialize WebDriver
         driver = WebDriverSetup.initializeDriver();
     }
 
     @Test
     public void testGoogleHomePage() {
+        logger.debug("Navigating to Google...");
         // Open Google homepage
         driver.get("https://www.google.com");
 
         // Verify the title of the page
         String pageTitle = driver.getTitle();
+        logger.debug("Page title is: " + pageTitle);
         Assert.assertEquals(pageTitle, "Google");
     }
 
@@ -47,5 +54,13 @@ public class SimpleTest {
         if (driver != null) {
             driver.quit();
         }
+    }
+
+    private void logTest(){
+        logger.trace("Testing logging...");
+        logger.debug("Testing logging...");
+        logger.info("Testing logging...");
+        logger.warn("Testing logging...");
+        logger.error("Testing logging...");
     }
 }
